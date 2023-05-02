c3ce2bef4fc7ae5b119182c08c742c5dc2cbbe53
vanGalilea <galiliziv@gmail.com>
2023-05-02 14:05:03 +0200

    adding ReactNativeFlipper.java

---
+++ examples/SampleApp/android/app/src/main/java/com/sampleapp/ReactNativeFlipper.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+package com.sampleapp;
+
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+
+/**
+ * Class responsible of loading Flipper inside your React Native application. This is the debug
+ * flavor of it. Here you can add your own plugins and customize the Flipper setup.
+ */
+public class ReactNativeFlipper {
+    public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+
+    }
+}
